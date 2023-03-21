442c44a2e60b71c08eac4401cda45043cdc6fcad
liuye <csu1704liuye@163.com>
2023-03-21 17:31:32 +0800

    Update

---
+++ signal_reconstruct_A/data_precessing.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2023/3/21 15:47
+# @Author  : LiuYe
+# @Email   : csu1704liuye@163.com | sy2113205@buaa.edu.cn
+# @File    : data_precessing.py
+# @Software: PyCharm
+import torch
+import torchaudio
+import torchaudio.functional as F
+import torchaudio.transforms as T
+
+print(torch.__version__)
+print(torchaudio.__version__)
+
+import math
+import timeit
+
+import librosa
+import resampy
+import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
+import pandas as pd
+from IPython.display import Audio, display
+
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+DEFAULT_OFFSET = 201
+
+
+def _get_log_freq(sample_rate, max_sweep_rate, offset):
+    """Get freqs evenly spaced out in log-scale, between [0, max_sweep_rate // 2]
+
+    offset is used to avoid negative infinity `log(offset + x)`.
+
+    """
+    start, stop = math.log(offset), math.log(offset + max_sweep_rate // 2)
+    return torch.exp(torch.linspace(start, stop, sample_rate, dtype=torch.double)) - offset
+
+
+def _get_inverse_log_freq(freq, sample_rate, offset):
+    """Find the time where the given frequency is given by _get_log_freq"""
+    half = sample_rate // 2
+    return sample_rate * (math.log(1 + freq / offset) / math.log(1 + half / offset))
+
+
+def _get_freq_ticks(sample_rate, offset, f_max):
+    # Given the original sample rate used for generating the sweep,
+    # find the x-axis value where the log-scale major frequency values fall in
+    times, freq = [], []
+    for exp in range(2, 5):
+        for v in range(1, 10):
+            f = v * 10**exp
+            if f < sample_rate // 2:
+                t = _get_inverse_log_freq(f, sample_rate, offset) / sample_rate
+                times.append(t)
+                freq.append(f)
+    t_max = _get_inverse_log_freq(f_max, sample_rate, offset) / sample_rate
+    times.append(t_max)
+    freq.append(f_max)
+    return times, freq
+
+
+def get_sine_sweep(sample_rate, offset=DEFAULT_OFFSET):
+    max_sweep_rate = sample_rate
+    freq = _get_log_freq(sample_rate, max_sweep_rate, offset)
+    delta = 2 * math.pi * freq / sample_rate
+    cummulative = torch.cumsum(delta, dim=0)
+    signal = torch.sin(cummulative).unsqueeze(dim=0)
+    return signal
+
+
+def plot_sweep(
+    waveform,
+    sample_rate,
+    title,
+    max_sweep_rate=48000,
+    offset=DEFAULT_OFFSET,
+):
+    x_ticks = [100, 500, 1000, 5000, 10000, 20000, max_sweep_rate // 2]
+    y_ticks = [1000, 5000, 10000, 20000, sample_rate // 2]
+
+    time, freq = _get_freq_ticks(max_sweep_rate, offset, sample_rate // 2)
+    freq_x = [f if f in x_ticks and f <= max_sweep_rate // 2 else None for f in freq]
+    freq_y = [f for f in freq if f in y_ticks and 1000 <= f <= sample_rate // 2]
+
+    figure, axis = plt.subplots(1, 1)
+    _, _, _, cax = axis.specgram(waveform[0].numpy(), Fs=sample_rate)
+    plt.xticks(time, freq_x)
+    plt.yticks(freq_y, freq_y)
+    axis.set_xlabel("Original Signal Frequency (Hz, log scale)")
+    axis.set_ylabel("Waveform Frequency (Hz)")
+    axis.xaxis.grid(True, alpha=0.67)
+    axis.yaxis.grid(True, alpha=0.67)
+    figure.suptitle(f"{title} (sample rate: {sample_rate} Hz)")
+    plt.colorbar(cax)
+    plt.show(block=True)
+
